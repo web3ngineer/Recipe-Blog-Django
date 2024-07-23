478cbdf040dca152e8dc34ae5fef1a71ae8f6119
Shvam Singh <singhshivam583@yahoo.in>
2024-07-23 18:32:19 +0530

    update

--- basic_setup_01/basic_setup_01/settings.py
+++ basic_setup_01/basic_setup_01/settings.py
@@ -125,11 +125,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public/staticmedia')
+
 # STATICFILES_DIRS = [ BASE_DIR / "static"]
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, "public/staticmedia")]
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'public/staticmedia')
 
 
 
--- basic_setup_01/recipeProject/models.py
+++ basic_setup_01/recipeProject/models.py
@@ -7,4 +7,4 @@ class Receipe(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     receipe_name = models.CharField(max_length=100)
     receipe_desc = models.TextField()
-    receipe_image = models.ImageField(upload_to="receipe")
+    receipe_image = models.ImageField(upload_to="receipe/")
